6274af89cbae6615093beda848c8aee1eb675b7e
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 04:15:23 +0200

    feat(homepage): add "add contact" card in homepage

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import Button from '../../components/button/button.component'
+
 import './homepage.styles.scss'
 
-const Homepage = () => (
-  <main>
-    <div className="container" />
-  </main>
-)
+const Homepage = () => {
+  const newContact = () => {
+    alert('Add new contact')
+  }
+
+  return (
+    <main>
+      <div className="container">
+        <div className="add-contact">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="butAdd" className="add-contact__plus">
+            +
+          </label>
+          <h4 className="add-contact__title"> Adicionar Contacto </h4>
+          <Button id="butAdd" content="Novo Contacto" type="white" handleClick={newContact} />
+        </div>
+      </div>
+    </main>
+  )
+}
 
 export default Homepage
