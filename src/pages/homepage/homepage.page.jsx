e4c409cfa481043f604f387aed4198ea31bf732f
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 20:41:41 +0200

    feat(homepage): add filter card

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Button from '../../components/button/button.component'
+import Input from '../../components/input/input.component'
 
 import './homepage.styles.scss'
 
@@ -19,6 +20,15 @@ const Homepage = () => {
           </label>
           <h4 className="add-contact__title"> Adicionar Contacto </h4>
           <Button id="butAdd" content="Novo Contacto" type="white" handleClick={newContact} />
+        </div>
+
+        <div className="filter">
+          <h1 className="filter__qty"> 45 </h1>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          <h2 className="filter__title"> Contactos <span>Adicionados</span> </h2>
+          <p className="filter__subtitle"> Uma plataforma especializada ma prospeção de novos clientes para o seu negócio </p>
+          <Input type="text" id="filter-name" label="Nome" />
+          <Input type="text" id="filter-company" label="Empresa" />
         </div>
       </div>
     </main>
