abb5b8d3cb226f48090515db01982356d0f269b6
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 03:50:02 +0200

    feat(button): add id

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 import './button.styles.scss'
 
-const Button = ({ content, type, handleClick }) => (
-  <button type="button" className={`btn btn--${type}`} onClick={handleClick}>
+const Button = ({
+  id,
+  content,
+  type,
+  handleClick
+}) => (
+  <button type="button" id={id} className={`btn btn--${type}`} onClick={handleClick}>
     {content}
   </button>
 )
