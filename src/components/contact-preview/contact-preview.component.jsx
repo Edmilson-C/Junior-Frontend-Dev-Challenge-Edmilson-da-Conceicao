788d43ee9cc6758bc8fe3310fc8b1885eb26031e
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-13 01:42:06 +0200

    feat: :sparkles: create contact preview component  & add person info component
    card thats shows preview of contact details

---
+++ src/components/contact-preview/contact-preview.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import PersonInfo from '../person-info/person-info.component'
+
+import './contact-preview.styles.scss'
+
+const ContactPreview = ({
+  imgUrl, name, email, empresa, website, codPostal, phoneNum
+}) => (
+  <div className="contact-preview">
+    <PersonInfo imgUrl={imgUrl} name={name} email={email} parent="preview" />
+    <div className="contact-preview__details">
+      <p className="contact-preview__detail">
+        <strong>Empresa: </strong>
+        {empresa}
+      </p>
+      <p className="contact-preview__detail">
+        <strong>Website: </strong>
+        {website}
+      </p>
+      <p className="contact-preview__detail">
+        <strong>Código Postal: </strong>
+        {codPostal}
+      </p>
+    </div>
+    <p className="contact-preview__number">{phoneNum}</p>
+  </div>
+)
+
+export default ContactPreview
