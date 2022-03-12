83957ecacf07a0918326cc35095e47804c9adc21
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 04:12:01 +0200

    add homepage to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/header/header.component'
+import Homepage from './pages/homepage/homepage.page'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Homepage />
     </div>
   )
 }
