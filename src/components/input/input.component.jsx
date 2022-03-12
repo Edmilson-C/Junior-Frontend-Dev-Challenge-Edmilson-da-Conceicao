5986bcb52734d258d8e06abc5ee8a33a304bf5f0
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 18:36:54 +0200

    feat: create input component

---
+++ src/components/input/input.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './input.styles.scss'
+
+const Input = ({
+  type, id, label, handleChange, ...otherProps
+}) => (
+  <div className="input-group">
+    <input type={type} id={id} className="input-group__input" onChange={handleChange} {...otherProps} />
+    <label htmlFor={id} className="input-group__label">
+      {label}
+    </label>
+  </div>
+)
+
+export default Input
