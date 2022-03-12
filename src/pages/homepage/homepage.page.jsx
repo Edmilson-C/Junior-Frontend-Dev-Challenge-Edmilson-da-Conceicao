1684d753d197e227c422aeb69b84729790274538
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-12 21:20:33 +0200

    feat(homepage): add about card

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react'
 
 import Button from '../../components/button/button.component'
 import Input from '../../components/input/input.component'
+
+import blackManImage from '../../assets/black-man.png'
 
 import './homepage.styles.scss'
 
@@ -24,11 +27,16 @@ const Homepage = () => {
 
         <div className="filter">
           <h1 className="filter__qty"> 45 </h1>
-          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
           <h2 className="filter__title"> Contactos <span>Adicionados</span> </h2>
           <p className="filter__subtitle"> Uma plataforma especializada ma prospeção de novos clientes para o seu negócio </p>
           <Input type="text" id="filter-name" label="Nome" />
           <Input type="text" id="filter-company" label="Empresa" />
+        </div>
+
+        <div className="about">
+          <h3 className="about__title"> Cuidamos do seu <span>Negocio.</span> </h3>
+          <p className="about__paragraph"> Uma plataforma especializada na prospecção de novos clientes para o seu negócio. </p>
+          <img src={blackManImage} alt="" className="about__image" />
         </div>
       </div>
     </main>
