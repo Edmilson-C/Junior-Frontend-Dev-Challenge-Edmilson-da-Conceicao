3f05cfa0fd4edb1333bd5ad92348752af7a0d06b
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-13 19:30:50 +0200

    feat: :sparkles: create contacts context

---
+++ src/context/contacts.context.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react'
+
+import { requestContacts } from '../apis/contacts.data'
+
+export const ContactsContext = createContext({
+  contacts: [],
+  getContacts: () => {}
+})
+
+export const ContactsProvider = ({ children }) => {
+  const [contacts, setContacts] = useState([])
+
+  const getContacts = async () => {
+    try {
+      const data = await requestContacts()
+      setContacts([...data])
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert(`An error has occured while request contacts \n${error}`)
+    }
+  }
+
+  return (
+    <ContactsContext.Provider value={{ contacts, getContacts }}>
+      {children}
+    </ContactsContext.Provider>
+  )
+}
