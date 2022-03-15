6893859168be2d712e48c48688cf31a98bd3af3c
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-16 01:37:09 +0200

    feat(homepage): request contacts from contacts context

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,14 +1,23 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 
 import Button from '../../components/button/button.component'
 import Input from '../../components/input/input.component'
+import ContactPreview from '../../components/contact-preview/contact-preview.component'
+
+import { ContactsContext } from '../../context/contacts.context'
 
 import blackManImage from '../../assets/black-man.png'
 
 import './homepage.styles.scss'
 
 const Homepage = () => {
+  const { contacts, getContacts } = useContext(ContactsContext)
+
+  useEffect(() => {
+    getContacts()
+  }, [])
+
   const newContact = () => {
     alert('Add new contact')
   }
@@ -26,16 +35,24 @@ const Homepage = () => {
         </div>
 
         <div className="filter">
-          <h1 className="filter__qty"> 45 </h1>
-          <h2 className="filter__title"> Contactos <span>Adicionados</span> </h2>
-          <p className="filter__subtitle"> Uma plataforma especializada ma prospeção de novos clientes para o seu negócio </p>
+          <h1 className="filter__qty">{contacts.length || '-'}</h1>
+          <h2 className="filter__title">
+            Contactos <span>Adicionados</span>
+          </h2>
+          <p className="filter__subtitle">
+            Uma plataforma especializada ma prospeção de novos clientes para o seu negócio
+          </p>
           <Input type="text" id="filter-name" label="Nome" />
           <Input type="text" id="filter-company" label="Empresa" />
         </div>
 
         <div className="about">
-          <h3 className="about__title"> Cuidamos do seu <span>Negocio.</span> </h3>
-          <p className="about__paragraph"> Uma plataforma especializada na prospecção de novos clientes para o seu negócio. </p>
+          <h3 className="about__title">
+            Cuidamos do seu <span>Negocio.</span>
+          </h3>
+          <p className="about__paragraph">
+            Uma plataforma especializada na prospecção de novos clientes para o seu negócio.
+          </p>
           <img src={blackManImage} alt="" className="about__image" />
         </div>
       </div>
