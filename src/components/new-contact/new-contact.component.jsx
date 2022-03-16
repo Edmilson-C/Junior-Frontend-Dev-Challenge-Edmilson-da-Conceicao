0ec7dc9ad03cfb29e508c755bbbe1161abc4d4ee
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-17 01:22:50 +0200

    feat(new contact): :sparkles: add new contact to contacts array in context

--- src/components/new-contact/new-contact.component.jsx
+++ src/components/new-contact/new-contact.component.jsx
@@ -1,15 +1,45 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 import Input from '../input/input.component'
 import Button from '../button/button.component'
 
+import { ContactsContext } from '../../context/contacts.context'
+
 import './new-contact.styles.scss'
 
 const NewContact = ({ isVisible, setVisibility }) => {
+  const { addContact } = useContext(ContactsContext)
+
+  const [rua, setRua] = useState('')
+  const [cidade, setCidade] = useState('')
+  const [newContact, setNewContact] = useState({
+    id: 0,
+    name: '',
+    email: '',
+    website: '',
+    empresa: '',
+    phoneNum: '',
+    location: '',
+    categorias: [],
+    codPostal: ''
+  })
+
+  let {
+    name, email, website, empresa, phoneNum, location, categorias, codPostal
+  } = newContact
+
   const handleChange = (event) => {
-    console.log(event.target.value)
+    const { id, value } = event.target
+
+    if (id === 'rua') {
+      setRua(value)
+    } else if (id === 'cidade') {
+      setCidade(value)
+    } else {
+      setNewContact({ ...newContact, [id]: value })
+    }
   }
 
   const handleClick = (event) => {
@@ -17,6 +47,11 @@ const NewContact = ({ isVisible, setVisibility }) => {
 
     if (id === 'new-contact__close') {
       setVisibility(false)
+    } else if (id === 'contact-button') {
+      location = `${rua}, ${cidade}`
+      setNewContact({ ...newContact, location })
+
+      addContact(newContact)
     }
   }
 
@@ -32,15 +67,15 @@ const NewContact = ({ isVisible, setVisibility }) => {
         <span onClick={handleClick}><i id="new-contact__close" className="fas fa-times new-contact__close" /></span>
 
         <form className="new-contact__form">
-          <Input type="text" id="name" name="name" label="Nome" handleChange={handleChange} />
-          <Input type="email" id="email" name="email" label="Email" handleChange={handleChange} />
-          <Input type="tel" id="phoneNum" name="phoneNum" label="Telefone" handleChange={handleChange} />
-          <Input type="url" id="website" name="website" label="Website" handleChange={handleChange} />
-          <Input type="text" id="empresa" name="empresa" label="Nome da empresa" handleChange={handleChange} />
+          <Input type="text" id="name" name="name" label="Nome" value={name} handleChange={handleChange} />
+          <Input type="email" id="email" name="email" label="Email" value={email} handleChange={handleChange} />
+          <Input type="tel" id="phoneNum" name="phoneNum" label="Telefone" value={phoneNum} handleChange={handleChange} />
+          <Input type="url" id="website" name="website" label="Website" value={website} handleChange={handleChange} />
+          <Input type="text" id="empresa" name="empresa" label="Nome da empresa" value={empresa} handleChange={handleChange} />
           <Input type="text" id="categorias" name="categorias" label="Categorias" handleChange={handleChange} />
-          <Input type="text" id="rua" name="rua" label="Rua" handleChange={handleChange} />
-          <Input type="text" id="cidade" name="cidade" label="Cidade" handleChange={handleChange} />
-          <Input type="number" id="codPostal" name="codPostal" label="Código Postal" handleChange={handleChange} />
+          <Input type="text" id="rua" name="rua" label="Rua" value={rua} handleChange={handleChange} />
+          <Input type="text" id="cidade" name="cidade" label="Cidade" value={cidade} handleChange={handleChange} />
+          <Input type="number" id="codPostal" name="codPostal" label="Código Postal" value={codPostal} handleChange={handleChange} />
         </form>
 
         <Button id="contact-button" content="Gravar Contacto" type="green" handleClick={handleClick} />
