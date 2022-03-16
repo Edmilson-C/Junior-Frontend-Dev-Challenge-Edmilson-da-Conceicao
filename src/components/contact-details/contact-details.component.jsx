1407319a2b9dc18858581c356bea458093b7a9d7
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-16 18:11:23 +0200

    feat(contact details): :sparkles: show component when visibility is true and close component close icon is clicked

--- src/components/contact-details/contact-details.component.jsx
+++ src/components/contact-details/contact-details.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PersonInfo from '../person-info/person-info.component'
 
@@ -14,34 +14,56 @@ const ContactDetails = ({
   phoneNum,
   location,
   codPostal,
-  categorias
-}) => (
-  <div className="contact-details">
-    <div className="contact-details__content">
-      <i className="fas fa-times contact-details__close" />
-      <i className="far fa-trash-alt contact-details__trash" />
-      <img src={ColabLogo} alt="Colab Pub Logo" className="contact-details__colab-logo" />
+  categorias,
+  isVisible,
+  setVisibility
+}) => {
+  const [isDeleteContactVisible, setIsDeleteContactVisible] = useState(false)
 
-      <PersonInfo imgUrl={imgUrl} name={name} email={email} parent="details" />
+  const handleClick = (event) => {
+    const { id } = event.target
 
-      <p className="contact-details__detail">
-        <i className="fas fa-home" />
-        <span>{empresa}</span>
-      </p>
-      <p className="contact-details__detail">
-        <i className="fas fa-phone-alt" />
-        <span>{phoneNum}</span>
-      </p>
-      <p className="contact-details__detail">
-        <i className="far fa-map" />
-        <span>{location}</span>
-      </p>
-      <p className="contact-details__detail">
-        <i className="far fa-image" />
-        <span>{codPostal}</span>
-      </p>
-    </div>
-  </div>
-)
+    if (id === 'contact-details__close') {
+      setVisibility(false)
+    } else {
+      console.log('Show delete dialog')
+    }
+  }
+
+  return (
+    isVisible && (
+      <div className="contact-details">
+        <div className="contact-details__content">
+          <span onClick={handleClick}>
+            <i id="contact-details__close" className="fas fa-times contact-details__close" />
+          </span>
+          <span onClick={handleClick}>
+            <i id="contact-details__trash" className="far fa-trash-alt contact-details__trash" />
+          </span>
+          <img src={ColabLogo} alt="Colab Pub Logo" className="contact-details__colab-logo" />
+
+          <PersonInfo imgUrl={imgUrl} name={name} email={email} parent="details" />
+
+          <p className="contact-details__detail">
+            <i className="fas fa-home" />
+            <span>{empresa}</span>
+          </p>
+          <p className="contact-details__detail">
+            <i className="fas fa-phone-alt" />
+            <span>{phoneNum}</span>
+          </p>
+          <p className="contact-details__detail">
+            <i className="far fa-map" />
+            <span>{location}</span>
+          </p>
+          <p className="contact-details__detail">
+            <i className="far fa-image" />
+            <span>{codPostal}</span>
+          </p>
+        </div>
+      </div>
+    )
+  )
+}
 
 export default ContactDetails
