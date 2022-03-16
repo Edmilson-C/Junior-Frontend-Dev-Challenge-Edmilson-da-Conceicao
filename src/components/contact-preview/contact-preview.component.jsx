5bf727bc6576081885398c05782f27bc044bf780
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-16 17:25:31 +0200

    feat(contact preview): :sparkles: show contact details component when minimize icon is clicked

--- src/components/contact-preview/contact-preview.component.jsx
+++ src/components/contact-preview/contact-preview.component.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react'
 
+import ContactDetails from '../contact-details/contact-details.component'
 import PersonInfo from '../person-info/person-info.component'
 
 import './contact-preview.styles.scss'
 
 const ContactPreview = ({
   imgUrl, name, email, empresa, website, codPostal, phoneNum
-}) => (
-  <div className="contact-preview">
-    <PersonInfo imgUrl={imgUrl} name={name} email={email} parent="preview" />
-    <div className="contact-preview__details">
-      <p className="contact-preview__detail">
-        <strong>Empresa: </strong>
-        {empresa}
-      </p>
-      <p className="contact-preview__detail">
-        <strong>Website: </strong>
-        {website}
-      </p>
-      <p className="contact-preview__detail">
-        <strong>Código Postal: </strong>
-        {codPostal}
-      </p>
+}) => {
+  const [isAddContactVisible, setIsAddContactVisible] = useState(false)
+
+  return (
+    <div className="contact-preview">
+      <ContactDetails isVisible={isAddContactVisible} setVisibility={setIsAddContactVisible} />
+      <PersonInfo imgUrl={imgUrl} name={name} email={email} parent="preview" />
+      <div className="contact-preview__details">
+        <p className="contact-preview__detail">
+          <strong>Empresa: </strong>
+          {empresa}
+        </p>
+        <p className="contact-preview__detail">
+          <strong>Website: </strong>
+          {website}
+        </p>
+        <p className="contact-preview__detail">
+          <strong>Código Postal: </strong>
+          {codPostal}
+        </p>
+      </div>
+      <p className="contact-preview__number">{phoneNum}</p>
+      <span onClick={() => { setIsAddContactVisible(true) }}><i className="fas fa-compress-alt contact-preview__view-contact" /></span>
     </div>
-    <p className="contact-preview__number">{phoneNum}</p>
-    <i className="fas fa-compress-alt contact-preview__view-contact" />
-  </div>
-)
+  )
+}
 
 export default ContactPreview
