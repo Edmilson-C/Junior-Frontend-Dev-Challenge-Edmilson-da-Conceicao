be79a0ad6eea48f61cbb4c2611b97b379c9180f2
edmilson-c <edmilson.conceicao09@gmail.com>
2022-03-17 09:30:46 +0200

    feat: :sparkles: create delete contact component

---
+++ src/components/delete-contact/delete-contact.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Button from '../button/button.component'
+
+import './delete-contact.styles.scss'
+
+const DeleteContact = () => {
+  const handleClick = () => {
+    console.log('button clicked!')
+  }
+
+  return (
+    <div className="delete-contact">
+      <div className="delete-contact__content">
+        <i className="far fa-times-circle delete-contact__close-icon" />
+        <h3 className="delete-contact__title">Apagar Contacto</h3>
+        <p className="delete-contact__subtitle">Tem certeza que deseja apagar o contacto?</p>
+        <div className="delete-contact__buttons">
+          <Button id="btnCancel" type="grey" content="Cancelar" handleClick={handleClick} />
+          <Button id="btnDelete" type="red" content="Sim, apagar" handleClick={handleClick} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteContact
